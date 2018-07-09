56737f85063260b9547989acf2ab4b9f222bc874
aamleshi <aamleshi@gmail.com>
2018-07-09 17:06:03 -0500

    Started work on DownloadMaf.py

---
+++ DownloadMaf.py
@@ -0,0 +1,15 @@
+import numpy as np
+import pandas as pd
+
+def main():
+    CancerTypes = ['BRCA', 'GBM', 'OV', 'LUAD', 'UCEC', 'KIRC',
+                    'HNSC', 'LGG', 'THCA', 'LUSC', 'PRAD', 'SKCM'
+                    'COAD', 'STAD', 'BLCA', 'LIHC', 'CESC', 'KIRP',
+                    'SARC', 'LAML', 'ESCA', 'PAAD', 'PCPG', 'READ',
+                    'TGCT', 'THYM', 'THYM', 'KICH', 'ACC', 'MESO',
+                    'UVM', 'DLBC', 'UCS', 'CHOL']
+    Pipelines = ['Muse', 'Mutect', 'Somatic Sniper', 'Varscan']
+    
+
+if __name__ == "__main__":
+    main()
